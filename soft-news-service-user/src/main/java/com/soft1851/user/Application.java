e20b7b971d3e39fb0b3dccee6364a79f95c3aff1
flygit <1224178565@qq.com>
2020-11-13 22:51:44 +0800

    构建模块工程四

---
+++ soft-news-service-user/src/main/java/com/soft1851/user/Application.java
@@ -0,0 +1,18 @@
+package com.soft1851.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xunmi
+ * @ClassName Application
+ * @Description TODO
+ * @Date 2020/11/13
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.soft1851.user.controller;
+
+import com.soft1851.api.controller.user.HelloControllerApi;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xunmi
+ * @ClassName HelloController
+ * @Description TODO
+ * @Date 2020/11/13
+ * @Version 1.0
+ **/
+
+@RestController
+public class HelloController implements HelloControllerApi {
+
+    @Override
+    public Object hello() {
+        return "hello";
+    }
+}

e5c693510785e9d8d79fd42567c77f74400e057e
flygit <1224178565@qq.com>
2020-11-14 13:21:36 +0800

    配置logback日志与多环境profile

--- soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
+++ soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.soft1851.user.controller;
 
 import com.soft1851.api.controller.user.HelloControllerApi;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 
 @RestController
+@Slf4j
 public class HelloController implements HelloControllerApi {
 
     @Override
     public Object hello() {
+        log.info("info: hello");
+        log.warn("info: hello");
+        log.error("info: hello");
         return "hello";
     }
 }

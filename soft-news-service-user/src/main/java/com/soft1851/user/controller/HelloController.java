9ee92b716fbaf9c0cfb3ea1f7c836bb8a07e9b94
flygit <1224178565@qq.com>
2020-11-14 13:35:51 +0800

    优雅地返回封装结果

--- soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
+++ soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.soft1851.user.controller;
 
 import com.soft1851.api.controller.user.HelloControllerApi;
+import com.soft1851.result.GraceResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,8 +21,6 @@ public class HelloController implements HelloControllerApi {
     @Override
     public Object hello() {
         log.info("info: hello");
-        log.warn("info: hello");
-        log.error("info: hello");
-        return "hello";
+        return GraceResult.ok("hello");
     }
 }

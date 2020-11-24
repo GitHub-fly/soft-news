3c7cd2563a28d6dec90e0bc4258284d1b1dbfd86
flygit <1224178565@qq.com>
2020-11-24 19:43:34 +0800

    文章服务模块准备

---
+++ soft-news-service-article/src/main/java/com/soft1851/article/ArticleApplication.java
@@ -0,0 +1,18 @@
+package com.soft1851.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xunmi
+ * @ClassName ArticleApplication
+ * @Description TODO
+ * @Date 2020/11/24
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class ArticleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleApplication.class,args);
+    }
+}

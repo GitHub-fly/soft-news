24198f38b13f05e2ba489974e62f09ee823b2e43
flygit <1224178565@qq.com>
2020-11-25 17:57:11 +0800

    阿里云文本检测

--- soft-news-service-article/src/main/java/com/soft1851/article/service/impl/ArticleServiceImpl.java
+++ soft-news-service-article/src/main/java/com/soft1851/article/service/impl/ArticleServiceImpl.java
@@ -4,6 +4,7 @@ import com.soft1851.article.mapper.ArticleMapper;
 import com.soft1851.article.mapper.ArticleMapperCustom;
 import com.soft1851.article.service.ArticleService;
 import com.soft1851.enums.ArticleAppointType;
+import com.soft1851.enums.ArticleReviewLevel;
 import com.soft1851.enums.ArticleReviewStatus;
 import com.soft1851.enums.YesOrNo;
 import com.soft1851.exception.GraceException;
@@ -11,7 +12,9 @@ import com.soft1851.pojo.Article;
 import com.soft1851.pojo.Category;
 import com.soft1851.pojo.bo.NewArticleBO;
 import com.soft1851.result.ResponseStatusEnum;
+import com.soft1851.utils.extend.AliTextReviewUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.n3r.idworker.Sid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,19 +27,21 @@ import java.util.Date;
 /**
  * @author xunmi
  */
+@Slf4j
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ArticleServiceImpl implements ArticleService {
-    private  final ArticleMapper articleMapper;
-    private  final Sid sid;
+    private final ArticleMapper articleMapper;
+    private final Sid sid;
     private final ArticleMapperCustom articleMapperCustom;
+    private final AliTextReviewUtil aliTextReviewUtil;
 
     @Override
     public void createArticle(NewArticleBO newArticleBO, Category category) {
         String articleId = sid.nextShort();
         Article article = new Article();
 
-        BeanUtils.copyProperties(newArticleBO,article);
+        BeanUtils.copyProperties(newArticleBO, article);
         article.setId(articleId);
         article.setCategoryId(category.getId());
         article.setArticleStatus(ArticleReviewStatus.REVIEWING.type);
@@ -45,15 +50,31 @@ public class ArticleServiceImpl implements ArticleService {
         article.setIsDelete(YesOrNo.NO.type);
         article.setCreateTime(new Date());
         article.setUpdateTime(new Date());
-        if (article.getIsAppoint().equals(ArticleAppointType.TIMING.type)){
+        if (article.getIsAppoint().equals(ArticleAppointType.TIMING.type)) {
             article.setPublishTime(newArticleBO.getPublishTime());
-        }else if(article.getIsAppoint().equals(ArticleAppointType.IMMEDIATELY.type)){
+        } else if (article.getIsAppoint().equals(ArticleAppointType.IMMEDIATELY.type)) {
             article.setPublishTime(new Date());
         }
 
         int res = articleMapper.insert(article);
-        if (res != 1){
+        if (res != 1) {
             GraceException.display(ResponseStatusEnum.ARTICLE_CREATE_ERROR);
+        }
+        // 通过阿里只能AI实现对文章文本的自动检测
+        String reviewResult = aliTextReviewUtil.reviewTextContent(newArticleBO.getTitle() + newArticleBO.getContent());
+        log.info("审核结果" + reviewResult);
+        if (ArticleReviewLevel.PASS.type.equalsIgnoreCase(reviewResult)) {
+            log.info("审核通过");
+            // 修改文章状态为：审核通过
+            this.updateArticleStatus(articleId, ArticleReviewStatus.SUCCESS.type);
+        } else if (ArticleReviewLevel.REVIEW.type.equalsIgnoreCase(reviewResult)) {
+            log.info("需要人工复审");
+            // 修改文章状态：需要人工复审
+            this.updateArticleStatus(articleId, ArticleReviewStatus.WAITING_MANUAL.type);
+        } else if (ArticleReviewLevel.BLOCK.type.equalsIgnoreCase(reviewResult)) {
+            log.info("审核不通过");
+            // 修改文章状态为审核不通过
+            this.updateArticleStatus(articleId, ArticleReviewStatus.FAILED.type);
         }
     }
 
@@ -67,7 +88,7 @@ public class ArticleServiceImpl implements ArticleService {
         Article pendingArticle = new Article();
         pendingArticle.setArticleStatus(pendingStatus);
 
-        int res = articleMapper.updateByExampleSelective(pendingArticle,example);
+        int res = articleMapper.updateByExampleSelective(pendingArticle, example);
         if (res != 1) {
             GraceException.display(ResponseStatusEnum.ARTICLE_REVIEW_ERROR);
         }
